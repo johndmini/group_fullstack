1d8f8fd19d0cee3e42c8fc2199feb3261766ded1
John David Delgado <johndavid@Johns-MacBook-Pro.local>
2022-04-07 00:42:40 -0400

    back end server initiated. Mongoose Database created, baseline routes and model schemas created. Get All and Post request are functional for both users and social event submissions

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-testing
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Database Connection Configuration --- Rename the database name to whatever you want currently set to 'socialevents'
+mongoose.connect(
+  'mongodb://localhost:27017/socialevents',
+  console.log('Connected to MongoDB')
+);
+
+app.use('/users', require('./routes/userRoutes'));
+app.use('/submissions', require('./routes/submissionRoutes'));
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  return res.send({ error: err.message });
+});
+
+app.listen(9000, () => {
+  console.log('Express server listening on port 9000');
+});
